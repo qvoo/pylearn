8e2c44708e2c54ba72b377c1db2aa58de01d1e16
Qvoo <110219260+qvoo@users.noreply.github.com>
2026-03-06 22:46:18 +0800

    Add JavaScript examples for variables and data types
    Add JavaScript code demonstrating variable declaration, data types, and comparisons.

---
+++ js.js
@@ -0,0 +1,50 @@
+console.log("这是我打印的)
+            //注释
+
+
+let a //声明变量
+a="我是a" //赋值变量
+console.log(a)
+
+let b ="第二个变量
+console.log(b)
+
+js 数据类型 基本数据类型 引用数据类型
+
+字符串类型 写在引号里面的都叫字符串
+
+未定义 
+let a;
+console.log(a); 
+
+空类型null
+let a=null
+console.log(a); 
+
+数组
+let a=[1,2,3,4]
+a[-1]不支持负索引
+
+获取长度
+a.length 
+
+对象{}
+let a={
+  name:"asd",
+  sex:"man",
+  age:"12
+}
+
+取值
+a.name
+
+a++
+++a
+
+let a="10"
+let b=10
+console.log(a==b) //输出true 在js里两个等号是判断值相等
+
+console.log(a===b)//三个等号是判断类型相等不相等
+
+！= //判断不等
